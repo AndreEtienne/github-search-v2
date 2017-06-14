a981e4a39e518614e68ed0996eddfa872c3f3ebd
Andre Etienne <andreetienne1987@gmail.com>
2017-06-14 15:33:53 +0200

    Add travis

---
+++ deployment_build.sh
@@ -0,0 +1,22 @@
+
+#!/usr/bin/env bash
+set -ve
+
+
+GH_PAGES_REPO="github.com:AndreEtienne/github-search-v2.git"
+NAME="$(git log --pretty=format:"%an" | head -n1)"
+EMAIL="$(git log --pretty=format:"%ae" | head -n1)"
+
+
+# Push static site to branch `gh-pages` of target repository
+npm run build
+cd dist
+git init
+git remote add origin "https://${GH_TOKEN}@${GH_REF}"
+git checkout --orphan gh-pages
+git config user.name "${NAME}"
+git config user.email "${EMAIL}"
+
+git add -A .
+git commit -am "Deploy to GitHub Pages from ${TRAVIS_COMMIT}"
+git push --force -u "https://${GH_TOKEN}@${GH_PAGES_REPO}" gh-pages
